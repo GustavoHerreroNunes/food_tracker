ea3fab076315ceec48422b17cd7b6b6fd96d5fad
GustavoHerreroNunes <ghnunes2010@gmail.com>
2023-11-24 11:22:51 -0300

    Add: different text colors to help with reading

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             username = input("Digite o nome de usuário: ")
             senha = input("Digite a senha: ")
             sistema.cadastrar_usuario(username, senha)
-            print("Usuário cadastrado com sucesso!")
+            print("\33[32mUsuário cadastrado com sucesso!\33[0m")
 
         elif escolha == "2":
             username_input = input("Digite o nome de usuário: ")
@@ -30,10 +30,10 @@ if __name__ == "__main__":
             id_sessao = sistema.autenticar_usuario(username_input, senha_input)
 
             if id_sessao:
-                print("\nUsuário autenticado com sucesso! ID de Sessão:", id_sessao)
+                print(f"\n\33[32mUsuário autenticado com sucesso! ID de Sessão:{id_sessao}\33[0m")
 
                 usuario_na_sessao = sistema.obter_usuario_por_sessao(id_sessao)
-                print("Usuário na sessão:", usuario_na_sessao.username)
+                print(f"Usuário na sessão: \33[1m{usuario_na_sessao.username}\33[0m")
 
                 while True:
                     print("\n1 - Today's Foods" + 
@@ -60,20 +60,20 @@ if __name__ == "__main__":
                                 data = datetime.now().strftime("%d/%m/%Y")
                                 consumos = registro.listar_alimentos_consumidos(data)
                                 if consumos:
-                                    print(f"\nAlimentos consumidos em {data}:")
+                                    print(f"\n\33[1mAlimentos consumidos em {data}:\33[0m")
                                     for consumo in consumos:
-                                        print(f"  {consumo['alimento']} - {consumo['quantidade_consumida']}g, {consumo['calorias_consumidas']}cal")
+                                        print(f"  \33[32m{consumo['alimento']}\33[0m - {consumo['quantidade_consumida']}g, {consumo['calorias_consumidas']}cal")
                                 else:
                                     print(f"\nNenhum alimento consumido. Que tal adicionar novos alimentos?")
                             elif escolha == "3":
                                 data = datetime.now().strftime("%d/%m/%Y")
                                 total_calorias = registro.calcular_calorias_por_dia(data)
-                                print(f"\nHoje você consumiu {total_calorias}cal.")
+                                print(f"\n\33[34mHoje você consumiu {total_calorias}cal.\33[0m")
                             elif escolha == "0":
                                 print("\nVoltando...")
                                 break
                             else:
-                                print("\nOpção inválida. Tente novamente.")
+                                print("\n\33[31Opção inválida. Tente novamente.\33[0m")
                     
                     elif escolha == "2":
                         while True:
@@ -84,21 +84,21 @@ if __name__ == "__main__":
 
                             if escolha == "1":
                                 todos_alimentos = alimentos.listar_nos()
-                                print("\nTodos os alimentos:")
+                                print("\n\33[1mTodos os alimentos:\33[0m")
                                 for alimento in todos_alimentos:
-                                    print(f"  {alimento['alimento']} - {alimento['calorias_por_grama']}cal/g")
+                                    print(f"  \33[32m{alimento['alimento']}\33[0m - {alimento['calorias_por_grama']}cal/g")
                             elif escolha == "2":
                                 nome_alimento = input("\nDigite o nome do alimento: ")
                                 resultado_busca = alimentos.buscar(nome_alimento)
                                 if resultado_busca:
-                                    print(f"\nAlimento encontrado: {resultado_busca.alimento} - {resultado_busca.calorias_por_grama}cal/g")
+                                    print(f"\n\33[34mAlimento encontrado: {resultado_busca.alimento} - {resultado_busca.calorias_por_grama}cal/g\33[0m")
                                 else:
-                                    print("\nAlimento não encontrado.")
+                                    print("\n\33[31mAlimento não encontrado.\33[0m")
                             elif escolha == "0":
                                 print("\nVoltando...")
                                 break
                             else:
-                                print("\nOpção inválida. Tente novamente.")
+                                print("\n\33[31Opção inválida. Tente novamente.\33[0m")
                     elif escolha == "3":
                         while True:
                             print("\n1 - Ver total de calorias consumidas em todos os dias" +
@@ -108,25 +108,29 @@ if __name__ == "__main__":
 
                             if escolha == "1":
                                 calorias_todos_dias = registro.calcular_calorias_todos_dias()
-                                print("\nTotal de calorias consumidas em todos os dias:")
+                                print("\n\33[1mTotal de calorias consumidas em todos os dias:\33[0m")
                                 for data, calorias in calorias_todos_dias.items():
-                                    print(f"  {data} - {calorias}cal")
+                                    print(f"  \33[32m{data}\33[0m - {calorias}cal")
                             elif escolha == "2":
-                                data = input("\nDigite a data (dd/mm/aaaa): ")
+                                data = input("\nDigite a data \33[1m(dd/mm/aaaa)\33[1m: ")
                                 calorias_por_dia = registro.calcular_calorias_por_dia(data)
-                                print(f"\n{data}: {calorias_por_dia}cal")
+                                print(f"\n\33[32m{data}\33[0m: {calorias_por_dia}cal")
                             elif escolha == "0":
                                 print("\nVoltando...")
                                 break
                             else:
-                                print("\nOpção inválida. Tente novamente.")
-                    else:
+                                print("\n\33[31mOpção inválida. Tente novamente.\33[0m")
+                    elif escolha == "0":
                         print("\nVoltando...")
                         break
+                    else:
+                        print("\n\33[31mOpção inválida. Tente novamente.\33[0m")
             else:
-                print("\nUsuário ou senha inválidos. Tente novamente.")
+                print("\n\33[31mUsuário ou senha inválidos. Tente novamente.\33[0m")
         elif escolha == "0":
             print("\nSaindo do sistema")
             break
+        else:
+            print("\n\33[31mOpção inválida. Tente novamente.\33[0m")
                     
 
